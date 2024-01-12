02c58b314db590adfa82a171257a7ee1424ea289
faskaa <fazilnb@code.edu.az>
2024-01-12 19:34:47 +0400

    Created the design of the Index page of the Home Page

--- BackEndProject/Program.cs
+++ BackEndProject/Program.cs
@@ -5,10 +5,20 @@ namespace BackEndProject
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
 
-            app.MapGet("/", () => "Hello World!");
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
+            app.UseStaticFiles();
             app.Run();
         }
     }
